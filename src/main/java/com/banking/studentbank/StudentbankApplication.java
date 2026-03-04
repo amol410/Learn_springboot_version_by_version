0d3eddd0babdf3b9a227c917b5ab7d444f149c0b
amol410 <shindeamol410@gmail.com>
2026-03-05 02:36:05 +0530

    first commit

---
+++ src/main/java/com/banking/studentbank/StudentbankApplication.java
@@ -0,0 +1,13 @@
+package com.banking.studentbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentbankApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/banking/studentbank/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.banking.studentbank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/hello")
+    public String hello(){
+        return "Bank API is running";
+    }
+}
